9796851552ff6e80f58ab1cfc38e6da52d2b1456
Vallikazam <103114407+Vallikazam@users.noreply.github.com>
2024-03-01 12:05:19 +0600

    Task.java

--- Task.java
+++ Task.java
@@ -3,12 +3,14 @@ class Task {
     private String description;
     private String deadline;
     private boolean isCompleted;
+    private int priority;
 
-    public Task(String name, String deadline, String description, boolean isCompleted){
+    public Task(String name, String deadline, String description, boolean isCompleted, int priority){
         this.name = name;
         this.deadline = deadline;
         this.description = description;
         this.isCompleted = false;
+        this.priority = priority;
     }
 
     public String getName() {
@@ -20,7 +22,12 @@ class Task {
     public boolean getState() {
         return isCompleted;
     }
-
+    public int getPriority(){
+        return priority;
+    }
+    public void setPriority(int priority){
+        this.priority = priority;
+    }
     public void updateTask() {
         isCompleted = true;
     }
